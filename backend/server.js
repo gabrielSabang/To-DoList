f7bf02ea771efd069e649c5eb43329f92504101e
Gabriel Sabang <gabrielsabang99@gmail.com>
2026-04-20 08:19:08 +0800

    backend fixes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import todoRoutes from './controller/todoController'
+import todoRoutes from './routes/Cardroutes.js'
 
 dotenv.config();
 const app = express();
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 5001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());   
+app.use(cors()); 
+
+app.use((req, res, next) => {
+  console.log(`Incoming: ${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/api/cards/", todoRoutes); 
 
